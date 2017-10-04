a9d18d1d316ba5831fce8a57601a4fe8bbdbf9cc
Phil Hargett <phil@haphazardhouse.net>
2017-10-03 20:05:15 -0700

    Basic error handling

--- lib/jsonrpc/jsonrpc_server.pl
+++ lib/jsonrpc/jsonrpc_server.pl
@@ -16,7 +16,8 @@
 
 :- meta_predicate
   method(:,:,?),
-  declared_method(?,:,?).
+  declared_method(?,:,?),
+  declared_method(?,?,:).
 
 :- dynamic jsonrpc_connection/4.
 
@@ -91,7 +92,6 @@ handle_connection(Server, Peer,StreamPair) :-
   handle_connection(Server, Peer,StreamPair).
 
 handle_message(Server, Peer,Out,Message) :-
-  info('Received message from %w: %w',[Peer,Message]),
   is_list(Message)
     -> handle_batch(Server, Peer, Out, Message)
     ; handle_notification_or_request(Server, Peer, Out, Message).
@@ -108,13 +108,13 @@ handle_notification_or_request(Server, Peer, Out, Message) :-
     ; handle_notification(Server, Peer,Out,Message).
 
 handle_notification(Server, _Peer, _Out, Request) :-
-  dispatch_method(Server, Request.method, Request.params, _Result).
+  dispatch_method(Server, Request.id, Request.method, Request.params, _Result).
 
-handle_request(Server, Peer, Out, Request) :-
-  info('Received request for %t with %t from %w: %w',[Request.method, Request.params, Peer,Request]),
-  info('Method %w and params %w', [Request.method, Request.params]),
-  dispatch_method(Server, Request.method, Request.params, Result),
-  Response = _{id: Request.id, result: Result },
+handle_request(Server, _Peer, Out, Request) :-
+  catch(
+      dispatch_method(Server, Request.id, Request.method, Request.params, Response),
+      Exception,
+      dispatch_exception(Server,Request.id,Exception,Response)),
   write_response(Out,Response).
 
 cleanup_connection(Server, Port, Peer, StreamPair) :-
@@ -137,11 +137,32 @@ method(Server, Method, Module:Handler) :-
   Clause = declared_method(Server, Method, Module:Handler),
   ( Clause ; asserta(Clause) ).
 
-dispatch_method(Server, MethodName, Params, Result) :-
+dispatch_method(Server, Id, MethodName, Params, Response) :-
   atom_string(Method,MethodName),
-  declared_method(_MServer:Server, _MMethod:Method, Module:Handler),
-  apply(Module:Handler,[Result,Params]).
+  (declared_method(_MServer:Server, _MMethod:Method, Module:Handler) ;
+    throw(unknown_method(Method))),
+  apply(Module:Handler,[Result,Params]),
+  Response = _{id: Id, result: Result }.
+
+:- dynamic declared_exception/3.
+
+dispatch_exception(Server, Id, Exception, Response) :-
+  declared_exception(Server, Exception, Module:Handler),!,
+  apply(Module:Handler, [Server, Exception, Error]),
+  Response = _{id: Id, error: Error}.
 
 % simple method to echo parameters; good for testing
 echo(Params,Params) :-
   info('Echoing %w', [Params]).
+
+declared_exception(_,unknown_method(_),jsonrpc_server:unknown_method).
+
+declared_exception(_,_,jsonrpc_server:unknown_error).
+
+unknown_method(_Server, unknown_method(Method),Error) :-
+  swritef(Message, "Unknown method: %w",[Method]),
+  Error = _{code: -32601, message: Message },!.
+
+unknown_error(_Server, Exception, Error) :-
+  error("An unknown error occurred: %t", [Exception]),
+  Error = _{code: -32000, message: "An unknown error occurred" }, !.
