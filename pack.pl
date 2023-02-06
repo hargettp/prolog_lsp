a89517ca817dd78ea041ba82fb6461091a23b622
Phil Hargett <phil@haphazardhouse.net>
2023-02-06 13:34:15 -0800

    fix repo reference

--- pack.pl
+++ pack.pl
@@ -1,9 +1,9 @@
 name(prolog_lsp).
 title('Implementation of Language Server Protocol for Prolog').
-version('0.0.1').
+version('0.0.2').
 author('Phil Hargett','phil@haphazardhouse.net').
 maintainer('Phil Hargett', 'phil@haphazardhouse.net').
 packager('Phil Hargett', 'phil@haphazardhouse.net').
-home('https://github.com/hargettp/prolog-lsp').
+home('https://github.com/hargettp/prolog_lsp').
 provides(prolog_lsp).
 requires(log4p).
