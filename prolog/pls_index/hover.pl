d64d56928ff8303d6864f4533a3aa4a71f07db92
Phil Hargett <phil@haphazardhouse.net>
2023-03-18 12:10:39 -0700

    add callable to hovers, provide default no doc hover

--- prolog/pls_index/hover.pl
+++ prolog/pls_index/hover.pl
@@ -31,18 +31,33 @@ hover_for_position(URI, Position, Hover) :-
   get_hover(Callable, Range, Hover),
   !.
 
+%! get_hover(+Callablle, +Range, -Hover) is nondet.
+%
+% Return a hover for the indicated range; will always
+% return minimal markup, even if no documentation available.
+%
 get_hover(Callable, Range, Hover) :-
   doc_comment(Callable, _FileName:_Line, _Summary, Comment),
-    comment_markup(Comment, Docs),
-    Hover = _{
-      range: Range,
-      contents: _{
-        kind: 'markdown',
-        value: Docs
-        }
-    }.
+  comment_markup(Callable, Comment, Docs),
+  Hover = _{
+    range: Range,
+    contents: _{
+      kind: 'markdown',
+      value: Docs
+      }
+  }.
 
-comment_markup(Comment, Markup) :-
+get_hover(Callable, Range, Hover) :-
+  swritef(Docs, "## %w\n*No documentation available.*",[Callable]),
+  Hover = _{
+    range: Range,
+    contents: _{
+      kind: 'markdown',
+      value: Docs
+      }
+  }.
+
+comment_markup(Callable, Comment, Markup) :-
   is_structured_comment(Comment, Prefixes),
   string_codes(Comment, Codes),
   indented_lines(Codes, Prefixes, Lines),
@@ -56,7 +71,10 @@ comment_markup(Comment, Markup) :-
     Strings
     ),
   with_output_to(string(Markup),
-    forall(member(String, Strings),writeln(String))
+    (
+      writef("## %w\n",[Callable]),
+      forall(member(String, Strings),writeln(String))
+      )
     ).
 
 format_doc_line(Line, String) :-
