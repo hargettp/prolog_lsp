a6f1d768ebb164aa3dd2e4746690639f53074239
Phil Hargett <phil@haphazardhouse.net>
2023-03-24 15:22:30 -0700

    bump version

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(prolog_lsp).
 title('Implementation of Language Server Protocol for Prolog').
-version('0.0.2').
+version('0.0.4').
 author('Phil Hargett','phil@haphazardhouse.net').
 maintainer('Phil Hargett', 'phil@haphazardhouse.net').
 packager('Phil Hargett', 'phil@haphazardhouse.net').
