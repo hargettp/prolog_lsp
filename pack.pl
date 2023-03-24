e32a512948f9988de656e7249f847bcca986b52d
Phil Hargett <phil@haphazardhouse.net>
2023-03-24 15:26:53 -0700

    add download URL

--- pack.pl
+++ pack.pl
@@ -1,10 +1,10 @@
 name(prolog_lsp).
 title('Implementation of Language Server Protocol for Prolog').
-version('0.0.4').
+version('0.0.5').
 author('Phil Hargett','phil@haphazardhouse.net').
 maintainer('Phil Hargett', 'phil@haphazardhouse.net').
 packager('Phil Hargett', 'phil@haphazardhouse.net').
 home('https://github.com/hargettp/prolog_lsp').
+download('https://github.com/hargettp/prolog_lsp/releases/*.zip')
 provides(prolog_lsp).
 requires(log4p).
-requires(lambda_abstractions).
