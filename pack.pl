89ffbfe3e368686715cd86e137e43dfdd0d14a41
Phil Hargett <phil@haphazardhouse.net>
2023-03-24 15:29:20 -0700

    typeo in pack.pl

--- pack.pl
+++ pack.pl
@@ -1,10 +1,10 @@
 name(prolog_lsp).
 title('Implementation of Language Server Protocol for Prolog').
-version('0.0.5').
+version('0.0.6').
 author('Phil Hargett','phil@haphazardhouse.net').
 maintainer('Phil Hargett', 'phil@haphazardhouse.net').
 packager('Phil Hargett', 'phil@haphazardhouse.net').
 home('https://github.com/hargettp/prolog_lsp').
-download('https://github.com/hargettp/prolog_lsp/releases/*.zip')
+download('https://github.com/hargettp/prolog_lsp/releases/*.zip').
 provides(prolog_lsp).
 requires(log4p).
